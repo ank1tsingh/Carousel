8142080f6ace7949bba15d9bc4944ffd448ebf57
Ankit <118796622+ank1tsingh@users.noreply.github.com>
2025-08-02 21:39:10 +0530

    Initial Commit

---
+++ Carousel/CarouselApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarouselApp.swift
+//  Carousel
+//
+//  Created by Ankit on 02/08/25.
+//
+
+import SwiftUI
+
+@main
+struct CarouselApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
